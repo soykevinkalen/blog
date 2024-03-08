4cee6a8890f0079ca1a39cbd799afad8420caa9c
Kevin <kevin.baez@jedify.io>
2024-03-08 14:43:30 -0300

    first commit

--- config/server.js
+++ config/server.js
@@ -7,17 +7,4 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  middleware: {
-    // Add the keys here
-    load: {
-      before: ['responseTime', 'logger', 'cors', 'responses', 'gzip'],
-      order: [
-        'Define the middlewares\' load order by putting their name in this array is the right order',
-      ],
-    },
-    settings: {
-      keys: ['FQQ9mJkAgXqmIbqNfdCofw==,pXqLdzCdbhwhkZmL9bF/zg==,PipqnvdVSHYWtodU8AiNag==,OnEEjlKgnytF6Tw/pxFRhw==']
-    },
-    // ...
-  },
 });
