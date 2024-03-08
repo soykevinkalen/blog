c5aa22221b8b7dfc7fe16f3fc223255950af2980
Kevin <kevin.baez@jedify.io>
2024-03-08 12:12:57 -0300

    secret key

--- config/server.js
+++ config/server.js
@@ -7,4 +7,17 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  middleware: {
+    // Add the keys here
+    load: {
+      before: ['responseTime', 'logger', 'cors', 'responses', 'gzip'],
+      order: [
+        'Define the middlewares\' load order by putting their name in this array is the right order',
+      ],
+    },
+    settings: {
+      keys: ['f50c6878d7cea063126c0fbcdad42bab5ff9e9af501f4505ee5fcf61c727f0c7'],
+    },
+    // ...
+  },
 });
